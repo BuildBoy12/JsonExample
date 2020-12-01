54d15900b9143d0114eebf0016e2ace282dc06e3
BuildBoy12 <buildboy12345@gmail.com>
2020-12-01 00:41:58 -0500

    Init Upload

---
+++ JsonIntro/Classes/MyClass.cs
@@ -0,0 +1,8 @@
+namespace JsonIntro.Classes
+{
+    public class MyClass
+    {
+        public string MyName { get; set; }
+        public int MyAge { get; set; }
+    }
+}
---
+++ JsonIntro/InitClass.cs
@@ -0,0 +1,39 @@
+﻿namespace JsonIntro
+{
+    using Classes;
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+
+    public class MainClass
+    {
+        private const int Age = 2;
+        private const string FileName = "MyFile";
+        private const string Name = "Error!Sans";
+
+        public static void Main() => WriteToFile(Name, Age, FileName);
+
+        private static void WriteToFile(string name, int age, string directory)
+        {
+            var myClass = new MyClass
+            {
+                MyName = name,
+                MyAge = age
+            };
+
+            string serialized = JsonConvert.SerializeObject(myClass);
+            File.WriteAllText(directory, serialized);
+            Console.WriteLine("Writing complete.");
+            ReadFromFile(directory);
+        }
+
+        private static void ReadFromFile(string directory)
+        {
+            string lines = File.ReadAllText(directory);
+            MyClass deserialized = JsonConvert.DeserializeObject<MyClass>(lines);
+            Console.WriteLine("Deserialized object:" +
+                              $"\nName: {deserialized.MyName}" +
+                              $"\nAge: {deserialized.MyAge}");
+        }
+    }
+}
